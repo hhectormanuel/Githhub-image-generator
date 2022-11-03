48680f77a9b5480c228ae508ccb5e9a7e9b2e8cc
hhectormanuel <hectorcena@live.com.mx>
2022-11-03 11:52:20 -0700

    changes

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,12 +3,25 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> build_grid()
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _]} = image
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.chunk_every(3)
+    #|> mirror_row()
+  end
 
-    [r,g,b]
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _] = row
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
+
+    %Identicon.Image{image | color: {r,g,b}}
   end
 
   def hash_input(input) do
